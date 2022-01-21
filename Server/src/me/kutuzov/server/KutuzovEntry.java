0b348f9a7bade6c0d7e4da65a9dbc9d77a788da0
PK2_Stimpy <31771533+PK2-Stimpy@users.noreply.github.com>
2022-01-21 12:25:11 +0100

    Uploaded project files

---
+++ Server/src/me/kutuzov/server/KutuzovEntry.java
@@ -0,0 +1,7 @@
+package me.kutuzov.server;
+
+public class KutuzovEntry {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/me/kutuzov/loader/Loader.java
@@ -0,0 +1,7 @@
+package me.kutuzov.loader;
+
+public class Loader {
+    public static void main(String[] args) {
+        System.out.println("Kutuzov's BotNet Loader");
+    }
+}
